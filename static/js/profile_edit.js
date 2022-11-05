199a86644dccb6ffc10bbc532f3ff2bb0cfa1344
suin <parksuin55@gmail.com>
2022-11-05 22:09:47 +0900

    Feat : 프로필 수정 기능 추가(이미지 수정 제외) #26
    1. 프로필 수정 기능 추가 - fullname, email
    2. 이미지 수정은 처리 아직 처리 안됨

--- static/js/profile_edit.js
+++ static/js/profile_edit.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("로딩되었음")
     handleMock()
-
 });
 async function handleMock() {
     const response = await fetch('http://127.0.0.1:8000/users/profile/1/', {
@@ -10,39 +9,45 @@ async function handleMock() {
         },
         method: 'GET',
     })
-     const response_json = await response.json()
-     console.log(response_json)
-     let user = response_json
+    const response_json = await response.json();
+    console.log(response_json);
+    let user = response_json;
 
-     let profile_edit_box = document.getElementById("profile_edit_box")
-     profile_edit_box.innerHTML = '';
-    let new_profile_edit_box = document.createElement('div');
-    new_profile_edit_box.className = 'profile_section';
-    new_profile_edit_box.innerHTML = `
-    <div id="profile_edit_box">
-                <div >
-                    <div class="profile_section" style="display: inline-block;">
-                        <div class="section_profile_box" style="margin-top: 50px; ">
-                            <img class="section_profile_img" src="src="http://127.0.0.1:8000${user['profile_image']}">
-                        </div> 
-                        <div style="margin-top: 15px; margin-bottom: 50px;">
-                            <input id="profile" type="file" multiple style="width: 200px;">      
-                        </div>
-                        <div class="textbox">
-                            <input type="text" class="form-control" placeholder="${user['fullname']}">
-                        </div>
-                        <div class="textbox" >
-                            <input type="text" class="form-control" placeholder="${user['username']}">
-                        </div>
-                        <div class="textbox" >
-                            <input type="text" class="form-control" placeholder="${user['email']}">
-                        </div>
-                        <button type="button" class="btn_profile_cancel btn btn-secondary" style="background-color:#121212; color: #ffff;" >Cancel</button>
-                        <button type="button" class="btn_profile_save btn btn-secondary" style=" margin-left:15px;"  >Save</button>
-                    </div>
-                </div>
-            </div>
-    `;
-    profile_edit_box.append(new_profile_edit_box);
+    document.getElementById("fullname").value = user['fullname'];
+    document.getElementById("username").value =user['username'];
+    document.getElementById("email").value = user['email'];
+    document.getElementById("profile_img").setAttribute("src","http://127.0.0.1:8000"+user['profile_image'])
+}
+document.getElementById("savebutton").addEventListener("click",function(){
+    handleUpdate();
 
+});
+async function handleUpdate() {
+    let fullname = document.getElementById("fullname").value;
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let fileField = document.querySelector('input[type="file"]').value;
+
+    // FormData에 저장
+    const profile_formData = new FormData();
+    profile_formData.append("username",username);
+    profile_formData.append("fullname",fullname);
+    profile_formData.append("email",email);
+    profile_formData.append("profile_image",fileField);
+
+   const response = await fetch('http://127.0.0.1:8000/users/profile/2/', {
+        headers: {
+            'content-type':'multipart/form-data;boundary=----WebKitFormBoundaryyEmKNDsBKjB7QEqu',
+            "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3NjU0OTQ4LCJpYXQiOjE2Njc2MTE3NDgsImp0aSI6ImIxN2I1MTI5MDAwODRmMzM5NTg0MDcwZWNmMjMwYTgxIiwidXNlcl9pZCI6MSwidXNlcm5hbWUiOiJtb3JyaXNlbGl6YWJldGgifQ.MTr-2f6F2DrxHiNhAe-VEMpYMg4iYOHJaz-ClXfdsTc"
+        },
+        method: 'PUT',
+        body: profile_formData,
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        console.log('성공:', result);
+    })
+    .catch((error) => {
+        console.error('실패:', error);
+    });
 }
