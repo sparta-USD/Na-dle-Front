b8507a8dc1248e46365a9b9bad81a386d72ee543
dy <kvkvd12@gmail.com>
2022-11-07 14:07:20 +0900

    Fix: 회원가입 response 예외처리 #35
    1. 빈칸이 존재할때 ( fullname, username, password, password2 ) 예외처리
    2. 아이디가 이미 존재할때 예외처리
    
    Close #35

--- static/js/signup.js
+++ static/js/signup.js
@@ -3,34 +3,39 @@ window.onload = () => {
 }
 
 async function handleSignup() {
+
     const fullname = document.getElementById("fullname").value
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
     const password2 = document.getElementById("password2").value
-
-
-    if (password === password2) {
-        
-        const response = await fetch("http://127.0.0.1:8000/users/signup/", {
-            headers: {
-                "content-type": "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "fullname": fullname,
-                "username": username,
-                "password": password,
-                "password2": password2
-            })
+    const response = await fetch("http://127.0.0.1:8000/users/signup/", {
+        headers: {
+            "content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "fullname": fullname,
+            "username": username,
+            "password": password,
+            "password2": password2
         })
-        alert("회원가입 되었습니다.")
-        location.href = "http://127.0.0.1:5500/login.html";
+    })
+
+    if (fullname === "" || username === "" || password === "" || password2 === "") {
+        alert("빈칸을 채워주세요.")
+    }
+    else if (response_json.message === "이미 존재"){
+        alert("아이디가 존재합니다.")
     }
     else {
-        alert("비밀번호를 확인해주세요!")
-
+        if (password !== password2) {
+            alert("비밀번호를 확인해주세요!")
+        }
+        else {
+            alert("회원가입 되었습니다.")
+            location.href = "http://127.0.0.1:5500/signin.html";
+        }
     }
-
 
 
 }
