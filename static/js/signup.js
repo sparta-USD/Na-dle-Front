f1670a02b6975dfa2cc0612fc9b17157c6e37d14
dy <kvkvd12@gmail.com>
2022-11-07 15:01:06 +0900

    Fix: 회원가입 response 예외처리 #35
    1. 아이디가 이미 존재할때 예외처리 ( response.status 띄워줌 )
    
    Close #35

--- static/js/signup.js
+++ static/js/signup.js
@@ -8,34 +8,34 @@ async function handleSignup() {
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
     const password2 = document.getElementById("password2").value
-    const response = await fetch("http://127.0.0.1:8000/users/signup/", {
-        headers: {
-            "content-type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({
-            "fullname": fullname,
-            "username": username,
-            "password": password,
-            "password2": password2
-        })
-    })
-
+    
+    
     if (fullname === "" || username === "" || password === "" || password2 === "") {
         alert("빈칸을 채워주세요.")
     }
-    else if (response_json.message === "이미 존재"){
-        alert("아이디가 존재합니다.")
+    else if (password !== password2) {
+            alert("비밀번호를 확인해주세요!")        
     }
     else {
-        if (password !== password2) {
-            alert("비밀번호를 확인해주세요!")
-        }
-        else {
+        const response = await fetch("http://127.0.0.1:8000/users/signup/", {
+            headers: {
+                "content-type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({
+                "fullname": fullname,
+                "username": username,
+                "password": password,
+                "password2": password2
+            })
+        })
+        if(response.ok){
             alert("회원가입 되었습니다.")
             location.href = "http://127.0.0.1:5500/signin.html";
+        }else{
+            const response_json = await response.json()
+            alert(response_json.message);
+            console.warn(`${response.status} 에러가 발생했습니다.`)
         }
     }
-
-
 }
